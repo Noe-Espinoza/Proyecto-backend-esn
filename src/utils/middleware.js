d55c58a655935fd77a68e60ed93ad8f5f4a3b0d6
Noe-Espinoza <116444335+Noe-Espinoza@users.noreply.github.com>
2023-05-18 17:33:23 -0300

    Se corrige error de middleware.js

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,6 +1,6 @@
 
 const checkEspecie= (_, res, next)=> {
-    res.json({message:'Si la mascota es hamster, conejo, cobayo, pez, huron, se clasifica la especie como Pequeñas Mascotas'});
+    console.log ('Si la mascota es hamster, conejo, cobayo, pez, huron, se clasifica la especie como Pequeñas Mascotas');
     next()
 }
         
